3a231bf1ae67bedab9fb12615c85dd7f4f5d9935
Denis Generalov <gd@rambler-co.ru>
2012-11-18 00:57:40 +0400

    split status module adapt some of function

--- salt/modules/status.py
+++ salt/modules/status.py
@@ -18,7 +18,7 @@ __opts__ = {}
 # TODO: Make this module support windows hosts
 # TODO: Make this module support BSD hosts properly, this is very Linux specific
 def __virtual__():
-    if 'Windows' in __grains__['os']:
+    if __grains__['os'] in ('Windows', 'FreeBSD'):
         return False
     return 'status'
 
